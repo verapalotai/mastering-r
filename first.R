c55386464033e9ed45ed9abecf1840707f42dab1
verapalotai <vera.palotai@gmail.com>
2020-04-15 13:41:46 +0000

    Reformatted script, added functions.

--- first.R
+++ first.R
@@ -1,21 +1,30 @@
-## task 1
-
 library(binancer)
-
-btcusd <- binance_coins_prices()[symbol == 'BTC', usd]
-
-## task 2
-
 library(jsonlite)
-
-usdhuf <- fromJSON(readLines('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF'))$rates$HUF
-
-results <- btcusd*usdhuf*0.42
-
-## task 3
 library(logger)
+library(checkmate)
+library(scales)
+log_threshold(TRACE)
 
 BITCOINS <- 0.42
 log_info('Number of Bitcoins: {BITCOINS}') # glue
 
-BITCOINS * btcusd * usdhuf
+get_bitcoin_price <- function() {
+  tryCatch(
+    btcusd <- binance_coins_prices()[symbol == 'BTC', usd],
+    error = function(e) get_bitcoin_price()
+  )
+}
+
+forint <- function(x){
+  dollar(x, prefix='', suffix=' Ft')
+}
+
+btcusd <- get_bitcoin_price()
+log_info('The value of 1 Bitcoin: {dollar(btcusd)}')
+asset_number(btcusd, lower=1000)
+
+usdhuf <- fromJSON(readLines('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF'))$rates$HUF
+log_info('The value of 1 USD: {forint(usdhuf)}')
+asset_number(usdhuf, lower=250, upper=500)
+
+log_eval(forint(BITCOINS * btcusd * usdhuf)) # TODO formatting
